599e97d3d4a224569481a4555835a96b7644c9b0
Family <albon.136546120855@2depedqc.ph>
2023-05-12 22:01:20 +0800

    Try & Except Finished!

--- simple_calculator.py
+++ simple_calculator.py
@@ -12,8 +12,14 @@ while True:
     # ask the user for its choice of operation and save the input
     print("Great Day, a precious well-being!")
     selection = input("Pick a basic math operation among these four (+|-|*|/): ")
-# use try and except function
-# ask the user for two variables to solve
+    # use try and except function
+    if selection in ('+', '-', '*', '/'):
+        # ask the user for two variables to
+        try:
+             var1 = float(input("Enter your first variable number: "))
+             var2 = float(input("Enter your second variable number:"))
+        except ValueError:
+            print("Oops! Take a look back for a second, there must be a problem.")
 # recognize the two input numbers and solve for the choice of operation
 # print the output
 # once again, ask the user if want to try again or not
