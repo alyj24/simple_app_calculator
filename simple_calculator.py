ed9de5b90c38e0471b62fc0cef2a2bc91c23e028
Family <albon.136546120855@2depedqc.ph>
2023-05-12 21:51:04 +0800

    Instruction

--- simple_calculator.py
+++ simple_calculator.py
@@ -2,3 +2,8 @@ print("Assignment 5".center(42, "="))
 print("\033[92m=" * 42)
 print("\033[94mCMPE-103-MODULE-3-EXCEPTION-HANDLING-IN-PYTHON")
 print("\033[92m=" * 42)
+
+# create a simple app calculator that will ask the user to choose four basic math operations and ask for two numbers,
+# the finally, will display the result.
+# last goal of the program is to ask whether the user want to try another problem or not.
+
