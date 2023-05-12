c0ac8b3ba2337a67da7026b68d949e190e92b5e7
Family <albon.136546120855@2depedqc.ph>
2023-05-12 22:11:30 +0800

    Colors added, Great!

--- simple_calculator.py
+++ simple_calculator.py
@@ -10,34 +10,34 @@ print("\033[92m=" * 42)
 # pseudocode
 while True:
     # ask the user for its choice of operation and save the input
-    print("Great Day, a precious well-being!")
-    selection = input("Pick a basic math operation among these four (+|-|*|/): ")
+    print("\033[91mGreat Day, a precious well-being!")
+    selection = input("\033[95mPick a basic math operation among these four (+|-|*|/): ")
     # use try and except function
     if selection in ('+', '-', '*', '/'):
         # ask the user for two variables to
         try:
-             var1 = float(input("Enter your first variable number: "))
-             var2 = float(input("Enter your second variable number:"))
+             var1 = float(input("\033[92mEnter your first variable number: "))
+             var2 = float(input("\033[93mEnter your second variable number:"))
         except ValueError:
             print("Oops! Take a look back for a second, there must be a problem.")
         # recognize the two input numbers and solve for the choice of operation
         if selection == '+':
             result1 = ((var1+var2))
         # print the output
-            print("The outcome of your input is: ", result1)
+            print("\033[91mThe outcome of your input is: ", result1)
         elif selection == '-':
             result2 = ((var1-var2))
-            print("The outcome of your input is: ", result2)
+            print("\033[92mThe outcome of your input is: ", result2)
         elif selection == '*':
             result3 = ((var1*var2))
-            print("The outcome of your input is: ", result3)
+            print("\033[93mThe outcome of your input is: ", result3)
         elif selection == '/':
             result4 = ((var1/var2))
-            print("The outcome of your input is: ", result4)
+            print("\033[94mThe outcome of your input is: ", result4)
         # once again, ask the user if want to try again or not
-        option = input("Nice choice of your operation and number! Do you want to try again? (yes, indeed/already satisfied): ")
+        option = input("\033[95mNice choice of your operation and number! Do you want to try again? (yes, indeed/already satisfied): ")
         if option == "already satisfied"
-            print("Thank you!")
+            print("\033[96mThank you!")
             break
     else:
-        print("Oh no! There must be a problem in your input to be invalid.")
+        print("\033[93mOh no! There must be a problem in your input to be invalid.")
